ad12dd7af4bb36dbd9e9fd7dbbf198d711dde7ba
Donovan Hutchinson <don@converser.io>
2013-04-27 22:10:13 +0100

    Updated styles for article

--- config.rb
+++ config.rb
@@ -6,11 +6,12 @@ css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+line_comments = false
 
 require 'animation'
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
